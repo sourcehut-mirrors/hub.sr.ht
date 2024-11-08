5800e3f2e5562430a195aad2f55055ff6ae88be7
Conrad Hoffmann <ch@bitfehler.net>
2024-11-08 13:17:58 +0100

    Reduce setup.py to a stub
    All configuration is now in pyproject.toml, all tooling set up to use
    it. The stub remains (for now) to execute the call to `make`, as such
    calls are not supported by pyproject.toml.

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from distutils.core import setup
+from setuptools import setup
 import subprocess
 import os
 import sys
@@ -13,34 +13,4 @@ subp = subprocess.run([make, "SRHT_PATH=" + srht_path])
 if subp.returncode != 0:
     sys.exit(subp.returncode)
 
-ver = os.environ.get("PKGVER") or subprocess.run(['git', 'describe', '--tags'],
-      stdout=subprocess.PIPE).stdout.decode().strip()
-
-setup(
-  name = 'hubsrht',
-  packages = [
-      'hubsrht',
-      'hubsrht.alembic',
-      'hubsrht.alembic.versions',
-      'hubsrht.blueprints',
-      'hubsrht.types',
-  ],
-  version = ver,
-  description = 'hub.sr.ht website',
-  author = 'Drew DeVault',
-  author_email = 'sir@cmpwn.com',
-  url = 'https://git.sr.ht/~sircmpwn/hub.sr.ht',
-  install_requires = ['srht', 'pyyaml'],
-  license = 'AGPL-3.0',
-  package_data={
-      'hubsrht': [
-          'templates/*.html',
-          'static/*',
-          'static/icons/*',
-      ]
-  },
-  scripts = [
-      'hubsrht-initdb',
-      'hubsrht-migrate',
-  ]
-)
+setup()
