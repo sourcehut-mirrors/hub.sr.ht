sourcehut-mirrors/hub.sr.ht

commit f15a6a0335a5853ad464df0e0baafd64564d17c3

Use ariadne-codegen for GraphQL clients

hubsrht/blueprints/mailing_lists.py
@@ -1,7 +1,7 @@
 import json
 from flask import Blueprint, render_template, request, redirect, url_for, abort
 from hubsrht.projects import ProjectAccess, get_project, get_project_or_redir
-from hubsrht.services import lists
+from hubsrht.services.lists import ListsClient, Visibility as ListVisibility
 from hubsrht.types import Event, EventType
 from hubsrht.types import MailingList, Visibility
 from srht.config import get_origin
@@ -12,6 +12,8 @@ from srht.search import search_by
 from srht.validation import Validation
 
 mailing_lists = Blueprint("mailing_lists", __name__)
+
+LIST_WEBHOOK_VERSION = 2
 
 @mailing_lists.route("/<owner>/<project_name>/lists")
 def lists_GET(owner, project_name):
@@ -42,13 +44,51 @@ def lists_GET(owner, project_name):
 @loginrequired
 def new_GET(owner, project_name):
     owner, project = get_project_or_redir(owner, project_name, ProjectAccess.write)
+    client = ListsClient()
+
     # TODO: Pagination
-    mls = lists.get_lists(owner)
-    mls = sorted(mls, key=lambda r: r["updated"], reverse=True)
+    cursor = None
+    lists = []
+    while True:
+        batch = client.get_lists(cursor).me.lists
+        lists.extend(batch.results)
+        cursor = batch.cursor
+        if not cursor:
+            break
+
+    lists = sorted(lists, key=lambda r: r.updated, reverse=True)
     existing = [l.remote_id for l in (MailingList.query
             .filter(MailingList.project_id == project.id)).all()]
     return render_template("mailing-list-new.html", view="new-resource",
-            owner=owner, project=project, lists=mls, existing=existing)
+            owner=owner, project=project, lists=lists, existing=existing)
+
+def finalize_add_list(client, owner, project, mailing_list):
+    ml = MailingList()
+    ml.remote_id = mailing_list.id
+    ml.project_id = project.id
+    ml.owner_id = project.owner_id
+    ml.name = mailing_list.name
+    ml.webhook_id = -1
+    ml.webhook_version = 0
+    ml.description = mailing_list.description
+    ml.visibility = Visibility(mailing_list.visibility.value)
+    db.session.add(ml)
+    db.session.flush()
+
+    webhook_url = (get_origin("hub.sr.ht", external=True) +
+           url_for("webhooks.project_mailing_list", list_id=ml.id))
+    ml.webhook_id = client.create_list_webhook(
+            list_id=mailing_list.id,
+            payload=ListsClient.event_webhook_query,
+            url=webhook_url).webhook.id
+
+    event = Event()
+    event.event_type = EventType.mailing_list_added 
+    event.mailing_list_id = ml.id
+    event.project_id = project.id
+    event.user_id = project.owner_id
+    db.session.add(event)
+    db.session.commit()
 
 def lists_from_template(owner, project, template):
     project_url = url_for("projects.summary_GET",
@@ -90,10 +130,12 @@ Mailing list for end-user discussion and questions related to the
     }
     template = templates[template]
 
+    client = ListsClient()
+
     for list_name in template:
         desc = descs[list_name]
         list_name = list_name.lower() # Per lists.sr.ht naming rules
-        mailing_list = lists.get_list(owner, list_name)
+        mailing_list = client.get_list(list_name).me.list
         if mailing_list is not None:
             in_project = [l.remote_id for l in (MailingList.query
                     .filter(MailingList.project_id == project.id)
@@ -101,43 +143,12 @@ Mailing list for end-user discussion and questions related to the
             if in_project:
                 continue
         else:
-            valid = Validation({
-                "name": list_name,
-                "description": desc,
-            })
-            mailing_list = lists.create_list(owner, valid)
-            if not mailing_list:
-                raise Exception(json.dumps(valid.response))
-
-        ml = MailingList()
-        ml.remote_id = mailing_list["id"]
-        ml.project_id = project.id
-        ml.owner_id = project.owner_id
-        ml.name = mailing_list["name"]
-        ml.webhook_id = -1
-        ml.webhook_version = 0
-        ml.description = mailing_list["description"]
-        if mailing_list["defaultACL"]["browse"]:
-            ml.visibility = Visibility.PUBLIC
-        else:
-            ml.visibility = Visibility.UNLISTED
-        db.session.add(ml)
-        db.session.flush()
-
-        try:
-            ml.webhook_id, ml.webhook_version = lists.create_list_webhook(
-                    owner, mailing_list["id"], ml.id)
-        except:
-            lists.delete_list(owner, mailing_list["id"])
-            raise
-
-        event = Event()
-        event.event_type = EventType.mailing_list_added 
-        event.mailing_list_id = ml.id
-        event.project_id = project.id
-        event.user_id = project.owner_id
-        db.session.add(event)
-        db.session.commit()
+            mailing_list = client.create_list(
+                name=list_name,
+                description=desc,
+                visibility=ListVisibility(project.visibility.value)
+            ).mailing_list
+        finalize_add_list(client, owner, project, mailing_list)
 
     return redirect(project_url)
 
@@ -147,31 +158,36 @@ def new_POST(owner, project_name):
     owner, project = get_project(owner, project_name, ProjectAccess.write)
     if project is None:
         abort(404)
+
+    client = ListsClient()
     valid = Validation(request)
+
     if "from-template" in valid:
         template = valid.require("template")
         valid.expect(template in ["public-inbox",
                 "announce-devel", "announce-devel-discuss"],
             "Invalid template selection")
         if not valid.ok:
-            mls = lists.get_lists(owner)
-            mls = sorted(mls, key=lambda r: r["updated"], reverse=True)
+            lists = sorted(lists, key=lambda r: r.updated, reverse=True)
             existing = [l.remote_id for l in (MailingList.query
                     .filter(MailingList.project_id == project.id)).all()]
-            return render_template("mailing-list-new.html",
-                    view="new-resource", owner=owner, project=project,
-                    lists=mls, existing=existing **valid.kwargs)
+            return render_template("mailing-list-new.html", view="new-resource",
+                    owner=owner, project=project, lists=lists,
+                    existing=existing, **valid.kwargs)
         return lists_from_template(owner, project, template)
     elif "create" in valid:
-        mailing_list = lists.create_list(owner, valid)
+        mailing_list = client.create_list(
+            name=valid.require("name"),
+            description=valid.optional("description"),
+            visibility=ListVisibility(project.visibility.value)
+        ).mailing_list
         if not valid.ok:
-            mls = lists.get_lists(owner)
-            mls = sorted(mls, key=lambda r: r["updated"], reverse=True)
+            lists = sorted(lists, key=lambda r: r.updated, reverse=True)
             existing = [l.remote_id for l in (MailingList.query
                     .filter(MailingList.project_id == project.id)).all()]
-            return render_template("mailing-list-new.html",
-                    view="new-resource", owner=owner, project=project,
-                    lists=mls, existing=existing, **valid.kwargs)
+            return render_template("mailing-list-new.html", view="new-resource",
+                    owner=owner, project=project, lists=lists,
+                    existing=existing, **valid.kwargs)
     else:
         list_name = None
         for field in valid.source:
@@ -180,47 +196,17 @@ def new_POST(owner, project_name):
                 break
         if not list_name:
             search = valid.optional("search")
-            mls = lists.get_lists(owner)
+            lists = sorted(lists, key=lambda r: r.updated, reverse=True)
             # TODO: Search properly
-            mls = filter(lambda r: search.lower() in r["name"].lower(), mls)
-            mls = sorted(mls, key=lambda r: r["updated"], reverse=True)
+            lists = filter(lambda r: search.lower() in r.name.lower(), lists)
             existing = [l.remote_id for l in (MailingList.query
                     .filter(MailingList.project_id == project.id)).all()]
-            return render_template("mailing-list-new.html",
-                    view="new-resource", owner=owner, project=project,
-                    lists=mls, existing=existing, search=search)
-        mailing_list = lists.get_list(owner, list_name)
+            return render_template("mailing-list-new.html", view="new-resource",
+                    owner=owner, project=project, lists=lists,
+                    existing=existing, **valid.kwargs)
+        mailing_list = client.get_list(list_name).me.list
 
-    ml = MailingList()
-    ml.remote_id = mailing_list["id"]
-    ml.project_id = project.id
-    ml.owner_id = project.owner_id
-    ml.name = mailing_list["name"]
-    ml.description = mailing_list["description"]
-    if mailing_list["defaultACL"]["browse"]:
-        ml.visibility = Visibility.PUBLIC
-    else:
-        ml.visibility = Visibility.UNLISTED
-    ml.webhook_id = -1
-    ml.webhook_version = 0
-    db.session.add(ml)
-    db.session.flush()
-
-    try:
-        ml.webhook_id, ml.webhook_version = lists.create_list_webhook(
-                owner, mailing_list["id"], ml.id)
-    except:
-        lists.delete_list(owner, mailing_list["id"])
-        raise
-
-    event = Event()
-    event.event_type = EventType.mailing_list_added 
-    event.mailing_list_id = ml.id
-    event.project_id = project.id
-    event.user_id = project.owner_id
-    db.session.add(event)
-    db.session.commit()
-
+    finalize_add_list(client, owner, project, mailing_list)
     return redirect(url_for("projects.summary_GET",
         owner=owner.canonical_name, project_name=project.name))
 
@@ -278,12 +264,13 @@ def delete_POST(owner, project_name, list_id):
     db.session.delete(mailing_list)
     db.session.commit()
 
-    lists.delete_list_webhook(owner, hook_id)
+    client = ListsClient()
+    client.delete_list_webhook(hook_id)
 
     valid = Validation(request)
     delete_remote = valid.optional("delete-remote") == "on"
     if delete_remote:
-        lists.delete_list(owner, list_id)
+        client.delete_list(list_id)
 
     return redirect(url_for("projects.summary_GET",
         owner=owner.canonical_name, project_name=project.name))

.gitignore
@@ -16,3 +16,12 @@ build
 hub.sr.ht-api
 *_gen.go
 api/graph/api/generated.go
+ariadne/*.toml
+hubsrht/services/builds/*.py
+hubsrht/services/git/*.py
+!hubsrht/services/git/webhooks.py
+hubsrht/services/hg/*.py
+!hubsrht/services/hg/webhooks.py
+hubsrht/services/lists/*.py
+hubsrht/services/todo/*.py
+!hubsrht/services/todo/webhooks.py

Makefile
@@ -12,18 +12,50 @@ MIGRATIONDIR=$(ASSETS)/migrations/$(SERVICE)
 SASSC?=sassc
 SASSC_INCLUDE=-I$(ASSETS)/scss/
 
+ARIADNE_CODEGEN=ariadne-codegen
+
 BINARIES=\
 	$(SERVICE)-api
 
-all: all-bin all-share
+all: all-bin all-share all-python
 
 install: install-bin install-share
 
-clean: clean-bin clean-share
+clean: clean-bin clean-share clean-python
 
 all-bin: $(BINARIES)
 
 all-share: static/main.min.css
+
+BUILDS_GRAPHQL_QUERIES != echo hubsrht/services/builds/*.graphql
+GIT_GRAPHQL_QUERIES != echo hubsrht/services/git/*.graphql
+HG_GRAPHQL_QUERIES != echo hubsrht/services/hg/*.graphql
+LISTS_GRAPHQL_QUERIES != echo hubsrht/services/lists/*.graphql
+TODO_GRAPHQL_QUERIES != echo hubsrht/services/todo/*.graphql
+
+ariadne/%.toml: ariadne/%.toml.in
+	sed -e 's:@ASSETS@:$(ASSETS):g' < $< > $@
+
+hubsrht/services/builds/__init__.py: $(BUILDS_GRAPHQL_QUERIES) ariadne/builds.toml
+	$(ARIADNE_CODEGEN) --config ariadne/builds.toml
+
+hubsrht/services/git/__init__.py: $(GIT_GRAPHQL_QUERIES) ariadne/git.toml
+	$(ARIADNE_CODEGEN) --config ariadne/git.toml
+
+hubsrht/services/hg/__init__.py: $(HG_GRAPHQL_QUERIES) ariadne/hg.toml
+	$(ARIADNE_CODEGEN) --config ariadne/hg.toml
+
+hubsrht/services/lists/__init__.py: $(LISTS_GRAPHQL_QUERIES) ariadne/lists.toml
+	$(ARIADNE_CODEGEN) --config ariadne/lists.toml
+
+hubsrht/services/todo/__init__.py: $(TODO_GRAPHQL_QUERIES) ariadne/todo.toml
+	$(ARIADNE_CODEGEN) --config ariadne/todo.toml
+
+all-python: hubsrht/services/builds/__init__.py
+all-python: hubsrht/services/git/__init__.py
+all-python: hubsrht/services/hg/__init__.py
+all-python: hubsrht/services/lists/__init__.py
+all-python: hubsrht/services/todo/__init__.py
 
 install-bin: all-bin
 	mkdir -p $(BINDIR)
@@ -45,9 +77,24 @@ clean-bin:
 clean-share:
 	rm -f static/main.min.css static/main.css
 
-.PHONY: all all-bin all-share
+clean-python:
+	./scripts/clean-python builds
+	./scripts/clean-python git
+	./scripts/clean-python hg
+	./scripts/clean-python lists
+	./scripts/clean-python todo
+	# TODO: Replace the script with the following once legacy webhook
+	# support is removed:
+	# rm -rf hubsrht/services/builds/*.py hubsrht/services/builds/__pycache__
+	# rm -rf hubsrht/services/git/*.py hubsrht/services/git/__pycache__
+	# rm -rf hubsrht/services/hg/*.py hubsrht/services/hg/__pycache__
+	# rm -rf hubsrht/services/lists/*.py hubsrht/services/lists/__pycache__
+	# rm -rf hubsrht/services/todo/*.py hubsrht/services/todo/__pycache__
+	rm -f ariadne/*.toml
+
+.PHONY: all all-bin all-share all-python
 .PHONY: install install-bin install-share
-.PHONY: clean clean-bin clean-share
+.PHONY: clean clean-bin clean-share clean-python
 
 static/main.css: scss/main.scss
 	mkdir -p $(@D)

ariadne/builds.toml.in
@@ -1,0 +1,15 @@
+[tool.ariadne-codegen]
+async_client = false
+client_name = "BuildsClient"
+schema_path = "@ASSETS@/builds.sr.ht.graphqls"
+queries_path = "hubsrht/services/builds/"
+target_package_path = "hubsrht/services/"
+target_package_name = "builds"
+plugins = ["srht.ariadne.AriadnePlugin"]
+
+[tool.ariadne-codegen.sourcehut]
+service_name = "builds.sr.ht"
+
+[tool.ariadne-codegen.scalars.Time]
+type = "datetime.datetime"
+parse = "srht.graphql.gql_time"

ariadne/git.toml.in
@@ -1,0 +1,15 @@
+[tool.ariadne-codegen]
+async_client = false
+client_name = "GitClient"
+schema_path = "@ASSETS@/git.sr.ht.graphqls"
+queries_path = "hubsrht/services/git/"
+target_package_path = "hubsrht/services/"
+target_package_name = "git"
+plugins = ["srht.ariadne.AriadnePlugin"]
+
+[tool.ariadne-codegen.sourcehut]
+service_name = "git.sr.ht"
+
+[tool.ariadne-codegen.scalars.Time]
+type = "datetime.datetime"
+parse = "srht.graphql.gql_time"

ariadne/hg.toml.in
@@ -1,0 +1,15 @@
+[tool.ariadne-codegen]
+async_client = false
+client_name = "HgClient"
+schema_path = "@ASSETS@/hg.sr.ht.graphqls"
+queries_path = "hubsrht/services/hg/"
+target_package_path = "hubsrht/services/"
+target_package_name = "hg"
+plugins = ["srht.ariadne.AriadnePlugin"]
+
+[tool.ariadne-codegen.sourcehut]
+service_name = "hg.sr.ht"
+
+[tool.ariadne-codegen.scalars.Time]
+type = "datetime.datetime"
+parse = "srht.graphql.gql_time"

ariadne/lists.toml.in
@@ -1,0 +1,15 @@
+[tool.ariadne-codegen]
+async_client = false
+client_name = "ListsClient"
+schema_path = "@ASSETS@/lists.sr.ht.graphqls"
+queries_path = "hubsrht/services/lists/"
+target_package_path = "hubsrht/services/"
+target_package_name = "lists"
+plugins = ["srht.ariadne.AriadnePlugin"]
+
+[tool.ariadne-codegen.sourcehut]
+service_name = "lists.sr.ht"
+
+[tool.ariadne-codegen.scalars.Time]
+type = "datetime.datetime"
+parse = "srht.graphql.gql_time"

ariadne/todo.toml.in
@@ -1,0 +1,15 @@
+[tool.ariadne-codegen]
+async_client = false
+client_name = "TodoClient"
+schema_path = "@ASSETS@/todo.sr.ht.graphqls"
+queries_path = "hubsrht/services/todo/"
+target_package_path = "hubsrht/services/"
+target_package_name = "todo"
+plugins = ["srht.ariadne.AriadnePlugin"]
+
+[tool.ariadne-codegen.sourcehut]
+service_name = "todo.sr.ht"
+
+[tool.ariadne-codegen.scalars.Time]
+type = "datetime.datetime"
+parse = "srht.graphql.gql_time"

commit f45fd3db27b21da1746daa2c6dc6ffbb7f62bfdf

mailing lists: reinstate use of an internal origin for list webhooks
Before f15a6a03, hut would use an internal origin towards itself to
setup list list webhooks, but this got changed by accident to using an
external origin.

This patch reinstates the previous behaviour.

Fixes: f15a6a03 ("Use ariadne-codegen for GraphQL clients")
Signed-off-by: Simon Martin <simon@nasilyan.com>

hubsrht/blueprints/mailing_lists.py
@@ -75,7 +75,7 @@ def finalize_add_list(client, owner, project, mailing_list):
     db.session.add(ml)
     db.session.flush()
 
-    webhook_url = (get_origin("hub.sr.ht", external=True) +
+    webhook_url = (get_origin("hub.sr.ht", external=False) +
            url_for("webhooks.project_mailing_list", list_id=ml.id))
     ml.webhook_id = client.create_list_webhook(
             list_id=ml.remote_id,