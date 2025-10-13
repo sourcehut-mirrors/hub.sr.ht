sourcehut-mirrors/hub.sr.ht

commit f15a6a0335a5853ad464df0e0baafd64564d17c3

Use ariadne-codegen for GraphQL clients

hubsrht/blueprints/webhooks.py
@@ -2,10 +2,17 @@ import email
 import html
 import json
 import re
+import hubsrht.services.todo.webhooks as todo_webhooks # XXX Legacy webhooks
 from datetime import datetime
 from flask import Blueprint, request, current_app
 from hubsrht.builds import submit_patchset
-from hubsrht.services import git, todo, lists
+from hubsrht.services.todo import TodoClient, SubmitCommentInput
+from hubsrht.services.todo import TicketStatus, TicketResolution
+from hubsrht.services.todo import GraphQLClientGraphQLMultiError
+from hubsrht.services.git import GitClient
+from hubsrht.services.lists import ListsClient, ToolIcon
+from hubsrht.services.lists import EventWebhook as ListEventWebhook
+from hubsrht.services.lists import WebhookEvent as ListWebhookEvent
 from hubsrht.trailers import commit_trailers
 from hubsrht.types import Event, EventType, MailingList, SourceRepo, RepoType
 from hubsrht.types import Tracker, User, Visibility
@@ -13,7 +20,7 @@ from srht.config import get_origin
 from srht.crypto import fernet, verify_request_signature
 from srht.database import db
 from srht.flask import csrf_bypass
-from srht.graphql import GraphQLError, Error
+from srht.graphql import InternalAuth, Error
 from srht.validation import Validation
 from urllib.parse import quote
 
@@ -62,7 +69,7 @@ def git_user(user_id):
             summary += f"Deleted local:{repo.id}/remote:{repo.remote_id}. Thanks!\n"
         return summary, 200
     else:
-        raise NotImplementedError()
+        raise NotImplementedError
 
 @csrf_bypass
 @webhooks.route("/webhooks/git-repo/<int:repo_id>", methods=["POST"])
@@ -107,18 +114,33 @@ def git_repo(repo_id):
         db.session.add(event)
         db.session.commit()
 
+        git_client = GitClient(auth=InternalAuth(pusher))
         for ref in payload["refs"]:
             old = (ref["old"] or {}).get("id")
             new = (ref["new"] or {}).get("id")
             if not old or not new:
                 continue # New ref, or ref deleted
-            for commit in reversed(git.log(pusher, repo, old, new)):
-                for trailer, value in commit_trailers(commit["message"]):
+
+            log = git_client.get_log(
+                username=repo.owner.username,
+                repo=repo.name,
+                from_=new,
+            ).user.repository.log
+            if not log or not any(log.results):
+                continue
+            commits = []
+            for c in log.results:
+                if c.id == old:
+                    break
+                commits.append(c)
+
+            for commit in reversed(commits):
+                for trailer, value in commit_trailers(commit.message):
                     _handle_commit_trailer(trailer, value, pusher, repo, commit)
 
         return "Thanks!"
     else:
-        raise NotImplementedError()
+        raise NotImplementedError
 
 _ticket_url_re = re.compile(
     rf"""
@@ -137,11 +159,11 @@ def _handle_commit_trailer(trailer, value, pusher, repo, commit):
         return
 
     if trailer == "Closes":
-        resolution = "CLOSED"
+        resolution = TicketResolution.CLOSED
     elif trailer == "Fixes":
-        resolution = "FIXED"
+        resolution = TicketResolution.FIXED
     elif trailer == "Implements":
-        resolution = "IMPLEMENTED"
+        resolution = TicketResolution.IMPLEMENTED
     elif trailer == "References":
         resolution = None
     else:
@@ -151,37 +173,67 @@ def _handle_commit_trailer(trailer, value, pusher, repo, commit):
     if not match:
         return
 
-    commit_message = html.escape(commit["message"].split("\n")[0])
-    commit_author = html.escape(commit["author"]["name"].strip())
-    commit_sha = commit["id"][:7]
+    commit_message = html.escape(commit.message.split("\n")[0])
+    commit_author = html.escape(commit.author.name.strip())
+    commit_sha = commit.id[:7]
     commit_url = repo.url() + f"/commit/{commit_sha}"
     comment = f"""\
 *{commit_author} referenced this ticket in commit [{commit_sha}].*
 
 [{commit_sha}]: {commit_url} "{commit_message}"\
 """
-    existing_comments, trackerId = todo.get_ticket_comments(
-        user=pusher,
-        owner=match["owner"],
-        tracker=match["tracker"],
-        ticket=int(match["ticket"]),
-    )
-    if comment in existing_comments:
-        # avoid duplicate comments
-        return
-    ticketId = int(match["ticket"])
+
+    auth = InternalAuth(pusher)
+    todo_client = TodoClient(auth)
+
+    # Don't create duplicate comments
+    tracker = todo_client.get_ticket_comments(
+            username=match["owner"][1:],
+            tracker=match["tracker"],
+            ticket_id=int(match["ticket"]),
+    ).user.tracker
+    ticket = tracker.ticket
+    for event in ticket.events.results:
+        for change in event.changes:
+            if not hasattr(change, "text"):
+                continue
+            if change.text == comment:
+                return
+
     try:
-        todo.update_ticket(pusher, trackerId, ticketId, comment, resolution)
-    except GraphQLError as err:
-        if not err.has(Error.ACCESS_DENIED):
+        comment = SubmitCommentInput(text=comment)
+        if resolution is not None:
+            comment.status = TicketStatus.RESOLVED
+            comment.resolution = resolution
+
+        todo_client.submit_comment(
+                tracker_id=tracker.id,
+                ticket_id=ticket.id,
+                comment=comment)
+    except GraphQLClientGraphQLMultiError as err:
+        print(err)
+        is_access_denied = False
+        for err in err.errors:
+            if err.extensions.get("code") == Error.ACCESS_DENIED:
+                is_access_denied = True
+        if not is_access_denied:
             raise
+
         # Try again without resolving the ticket, in case this user has comment
         # access but not triage access
         try:
-            todo.update_ticket(pusher, trackerId, ticketId, comment)
-        except GraphQLError as err:
+            todo_client.submit_comment(
+                    tracker_id=tracker.id,
+                    ticket_id=ticket.id,
+                    comment=SubmitCommentInput(text=comment),
+            )
+        except GraphQLClientGraphQLMultiError as err:
+            is_access_denied = False
+            for err in err.errors:
+                if err.extensions.get("code") == Error.ACCESS_DENIED:
+                    is_access_denied = True
             # Silently discard further access denied errors
-            if not err.has(Error.ACCESS_DENIED):
+            if not is_access_denied:
                 raise
 
 @csrf_bypass
@@ -227,79 +279,78 @@ def hg_user(user_id):
 def project_mailing_list(list_id):
     event = request.headers.get("X-Webhook-Event")
     payload = verify_request_signature(request)
-    payload = json.loads(payload.decode('utf-8'))["data"]["webhook"]
+    payload = json.loads(payload.decode('utf-8'))["data"]
+    webhook = ListEventWebhook.model_validate(payload).webhook
 
     mailing_list = (MailingList.query
              .filter(MailingList.id == list_id)).one_or_none()
     if not mailing_list:
         return "I don't recognize that mailing list.", 404
 
-    if event == "LIST_UPDATED":
-        data = payload["list"]
+    match webhook.event:
+        case ListWebhookEvent.LIST_UPDATED:
+            mailing_list.name = webhook.list.name
+            mailing_list.description = webhook.list.description
+            mailing_list.visibility = Visibility(webhook.list.visibility.value)
+            mailing_list.project.updated = datetime.utcnow()
+            db.session.commit()
 
-        mailing_list.name = data["name"]
-        mailing_list.description = data["description"]
-        mailing_list.visibility = Visibility(data["visibility"])
-        mailing_list.project.updated = datetime.utcnow()
-        db.session.commit()
+            local_id = mailing_list.id
+            remote_id = mailing_list.remote_id
+            return f"Updated local:{local_id}/remote:{remote_id}", 200
+        case ListWebhookEvent.LIST_DELETED:
+            local_id = mailing_list.id
+            remote_id = mailing_list.remote_id
+            mailing_list.project.updated = datetime.utcnow()
+            db.session.delete(mailing_list)
+            db.session.commit()
+            return f"Deleted mailing lists local:{local_id}/remote:{remote_id}", 200
+        case ListWebhookEvent.EMAIL_RECEIVED:
+            email = webhook.email
+            sender_canon = email.sender.canonical_name
+            if hasattr(email.sender, "username"):
+                sender_username = email.sender.username
+            else:
+                sender_username = None
 
-        local_id = mailing_list.id
-        remote_id = mailing_list.remote_id
-        return f"Updated local:{local_id}/remote:{remote_id}", 200
-    elif event == "LIST_DELETED":
-        local_id = mailing_list.id
-        remote_id = mailing_list.remote_id
-        mailing_list.project.updated = datetime.utcnow()
-        db.session.delete(mailing_list)
-        db.session.commit()
-        return f"Deleted mailing lists local:{local_id}/remote:{remote_id}", 200
-    elif event == "EMAIL_RECEIVED":
-        data = payload["email"]
-        sender_canon = data["sender"]["canonicalName"]
-        sender_username = data["sender"].get("username")
-        subject = data["subject"]
-        message_id = f"<{data['messageID']}>"
-        archive_url = f"{mailing_list.url()}/{quote(message_id)}"
+            subject = email.subject
+            message_id = f"<{email.message_id}>"
+            archive_url = f"{mailing_list.url()}/{quote(message_id)}"
 
-        event = Event()
-        if sender_username:
-            sender = current_app.oauth_service.lookup_user(sender_username)
-            event.user_id = sender.id
-            attrib = f"<a href='{_listssrht}/{sender_canon}'>{sender_canon}</a>"
-        else:
-            attrib = sender_canon
+            event = Event()
+            if sender_username:
+                sender = current_app.oauth_service.lookup_user(sender_username)
+                event.user_id = sender.id
+                attrib = f"<a href='{_listssrht}/{sender_canon}'>{sender_canon}</a>"
+            else:
+                attrib = sender_canon
 
-        event.event_type = EventType.external_event
-        event.mailing_list_id = mailing_list.id
-        event.project_id = mailing_list.project_id
+            event.event_type = EventType.external_event
+            event.mailing_list_id = mailing_list.id
+            event.project_id = mailing_list.project_id
 
-        event.external_source = "lists.sr.ht"
-        event.external_summary = f"<a href='{archive_url}'>{html.escape(subject)}</a>"
-        event.external_details = (f"{attrib} via " +
-                f"<a href='{mailing_list.url()}'>{mailing_list.name}</a>")
-        event.external_url = archive_url
+            event.external_source = "lists.sr.ht"
+            event.external_summary = f"<a href='{archive_url}'>{html.escape(subject)}</a>"
+            event.external_details = (f"{attrib} via " +
+                    f"<a href='{mailing_list.url()}'>{mailing_list.name}</a>")
+            event.external_url = archive_url
 
-        db.session.add(event)
-        db.session.commit()
-        return f"Assigned event ID {event.id}"
-    elif event == "PATCHSET_RECEIVED":
-        data = payload["patchset"]
-        valid = Validation(request)
-        job_ids = []
+            db.session.add(event)
+            db.session.commit()
+            return f"Assigned event ID {event.id}"
+        case ListWebhookEvent.PATCHSET_RECEIVED:
+            patchset = webhook.patchset
+            job_ids = []
 
-        try:
-            ids = submit_patchset(mailing_list, data, valid)
+            ids = submit_patchset(mailing_list, patchset)
             if ids is not None:
                 job_ids.extend(ids)
-        except Exception as ex:
-            return "Error submitting builds: " + str(ex)
+            try:
+                pass
+            except Exception as ex:
+                return "Error submitting builds: " + str(ex)
 
-        if not valid.ok:
-            return valid.response
-        else:
             return "Submitted builds: " + ", ".join([str(x) for x in job_ids])
-    else:
-        raise Exception(f"Unknown mailing list webhook event: {event}")
 
 @csrf_bypass
 @webhooks.route("/webhooks/todo-user/<int:user_id>", methods=["POST"])
@@ -388,7 +439,7 @@ def todo_tracker(tracker_id):
 
         db.session.add(event)
         db.session.commit()
-        todo.ensure_ticket_webhooks(tracker, ticket_id)
+        todo_webhooks.ensure_ticket_webhooks(tracker, ticket_id)
         return "Thanks!"
     else:
         raise NotImplementedError()
@@ -469,13 +520,25 @@ def build_complete(details):
     if payload["owner"]["canonical_name"] != details["user"]:
         return "Discarding webhook from unauthorized build", 401
 
-    buildsrht = get_origin("builds.sr.ht", external=True)
-    build_url = f"{buildsrht}/{project.owner.canonical_name}/job/{payload['id']}"
+    builds_origin = get_origin("builds.sr.ht", external=True)
+    build_url = f"{builds_origin}/{project.owner.canonical_name}/job/{payload['id']}"
 
-    tool_details = f"[#{payload['id']}]({build_url}) {details['name']} {payload['status']}"
-    status = payload["status"].upper()
-    if status == 'TIMEOUT':
-        status = 'FAILED'
-    lists.patchset_update_tool(ml.owner, details["tool_id"], status, tool_details)
+    lists_client = ListsClient()
+    # TODO: Update me once builds.sr.ht adds a native enum for this
+    match payload["status"]:
+        case 'pending':
+            icon = ToolIcon.PENDING
+        case 'queued' | 'running':
+            icon = ToolIcon.WAITING
+        case 'success':
+            icon = ToolIcon.SUCCESS
+        case 'failed' | 'timeout':
+            icon = ToolIcon.FAILED
+        case 'cancelled':
+            icon = ToolIcon.CANCELLED
+
+    status_details = f"[#{payload['id']}]({build_url}) {details['name']} {payload['status']}"
+    lists_client.update_tool(tool_id=details["tool_id"],
+            icon=icon, details=status_details)
 
     return "Thanks!"

hubsrht/services/git/webhooks.py
@@ -1,0 +1,42 @@
+from flask import url_for
+from srht.config import get_origin
+from srht.api import ensure_webhooks
+
+_origin = get_origin("hub.sr.ht")
+_gitsrht = get_origin("git.sr.ht", default=None)
+
+def ensure_user_webhooks(user):
+    config = {
+        _origin + url_for("webhooks.git_user", user_id=user.id):
+            ["repo:update", "repo:delete"],
+    }
+    ensure_webhooks(user, f"{_gitsrht}/api/user/webhooks", config)
+
+def unensure_user_webhooks(user):
+    config = {
+        _origin + url_for("webhooks.git_user", user_id=user.id): None
+    }
+    try:
+        ensure_webhooks(user, f"{_gitsrht}/api/user/webhooks", config)
+    except:
+        pass # nbd, upstream was probably deleted
+
+def ensure_repo_webhooks(repo):
+    config = {
+        _origin + url_for("webhooks.git_repo", repo_id=repo.id):
+            ["repo:post-update"],
+    }
+    owner = repo.owner
+    url = f"{_gitsrht}/api/{owner.canonical_name}/repos/{repo.name}/webhooks"
+    ensure_webhooks(owner, url, config)
+
+def unensure_repo_webhooks(repo):
+    config = {
+        _origin + url_for("webhooks.git_repo", repo_id=repo.id): None
+    }
+    owner = repo.owner
+    url = f"{_gitsrht}/api/{owner.canonical_name}/repos/{repo.name}/webhooks"
+    try:
+        ensure_webhooks(owner, url, config)
+    except:
+        pass # nbd, upstream was presumably deleted

hubsrht/services/hg/webhooks.py
@@ -1,0 +1,22 @@
+from flask import url_for
+from srht.config import get_origin
+from srht.api import ensure_webhooks
+
+_origin = get_origin("hub.sr.ht")
+_hgsrht = get_origin("hg.sr.ht", default=None)
+
+def ensure_user_webhooks(user):
+    config = {
+        _origin + url_for("webhooks.hg_user", user_id=user.id):
+            ["repo:update", "repo:delete"],
+    }
+    ensure_webhooks(user, f"{_hgsrht}/api/user/webhooks", config)
+
+def unensure_user_webhooks(user):
+    config = {
+        _origin + url_for("webhooks.hg_user", user_id=user.id): None
+    }
+    try:
+        ensure_webhooks(user, f"{_hgsrht}/api/user/webhooks", config)
+    except:
+        pass # nbd, upstream was presumably deleted

hubsrht/services/todo/webhooks.py
@@ -1,0 +1,62 @@
+from flask import url_for
+from srht.config import get_origin
+from srht.api import ensure_webhooks
+
+_origin = get_origin("hub.sr.ht")
+_todosrht = get_origin("todo.sr.ht", default=None)
+
+def ensure_user_webhooks(user):
+    config = {
+        _origin + url_for("webhooks.todo_user", user_id=user.id):
+            ["tracker:update", "tracker:delete"]
+    }
+    url = f"{_todosrht}/api/user/webhooks"
+    ensure_webhooks(user, url, config)
+
+def unensure_user_webhooks(user):
+    config = {
+         _origin + url_for("webhooks.todo_user", user_id=user.id): None
+    }
+    url = f"{_todosrht}/api/user/webhooks"
+    try:
+        ensure_webhooks(user, url, config)
+    except:
+        pass # nbd, upstream was presumably deleted
+
+def ensure_tracker_webhooks(tracker):
+    config = {
+        _origin + url_for("webhooks.todo_tracker", tracker_id=tracker.id):
+            ["ticket:create"]
+    }
+    owner = tracker.owner
+    url = f"{_todosrht}/api/user/{owner.canonical_name}/trackers/{tracker.name}/webhooks"
+    ensure_webhooks(owner, url, config)
+
+def unensure_tracker_webhooks(tracker):
+    config = {
+        _origin + url_for("webhooks.todo_tracker", tracker_id=tracker.id): None
+    }
+    owner = tracker.owner
+    url = f"{_todosrht}/api/user/{owner.canonical_name}/trackers/{tracker.name}/webhooks"
+    try:
+        ensure_webhooks(owner, url, config)
+    except:
+        pass # nbd, upstream was presumably deleted
+
+def ensure_ticket_webhooks(tracker, ticket_id):
+    config = {
+        _origin + url_for("webhooks.todo_ticket", tracker_id=tracker.id):
+            ["event:create"]
+    }
+    owner = tracker.owner
+    url = f"{_todosrht}/api/user/{owner.canonical_name}/trackers/{tracker.name}/tickets/{ticket_id}/webhooks"
+    ensure_webhooks(owner, url, config)
+
+def unensure_ticket_webhooks(tracker, ticket_id):
+    config = { }
+    owner = tracker.owner
+    url = f"{_todosrht}/api/user/{owner.canonical_name}/trackers/{tracker.name}/tickets/{ticket_id}/webhooks"
+    try:
+        ensure_webhooks(owner, url, config)
+    except:
+        pass # nbd, upstream was presumably deleted

.gitignore
@@ -16,3 +16,12 @@ build
 hub.sr.ht-api
 *_gen.go
 api/graph/api/generated.go
+ariadne/*.toml
+hubsrht/services/builds/*.py
+hubsrht/services/git/*.py
+!hubsrht/services/git/webhooks.py
+hubsrht/services/hg/*.py
+!hubsrht/services/hg/webhooks.py
+hubsrht/services/lists/*.py
+hubsrht/services/todo/*.py
+!hubsrht/services/todo/webhooks.py

Makefile
@@ -12,18 +12,50 @@ MIGRATIONDIR=$(ASSETS)/migrations/$(SERVICE)
 SASSC?=sassc
 SASSC_INCLUDE=-I$(ASSETS)/scss/
 
+ARIADNE_CODEGEN=ariadne-codegen
+
 BINARIES=\
 	$(SERVICE)-api
 
-all: all-bin all-share
+all: all-bin all-share all-python
 
 install: install-bin install-share
 
-clean: clean-bin clean-share
+clean: clean-bin clean-share clean-python
 
 all-bin: $(BINARIES)
 
 all-share: static/main.min.css
+
+BUILDS_GRAPHQL_QUERIES != echo hubsrht/services/builds/*.graphql
+GIT_GRAPHQL_QUERIES != echo hubsrht/services/git/*.graphql
+HG_GRAPHQL_QUERIES != echo hubsrht/services/hg/*.graphql
+LISTS_GRAPHQL_QUERIES != echo hubsrht/services/lists/*.graphql
+TODO_GRAPHQL_QUERIES != echo hubsrht/services/todo/*.graphql
+
+ariadne/%.toml: ariadne/%.toml.in
+	sed -e 's:@ASSETS@:$(ASSETS):g' < $< > $@
+
+hubsrht/services/builds/__init__.py: $(BUILDS_GRAPHQL_QUERIES) ariadne/builds.toml
+	$(ARIADNE_CODEGEN) --config ariadne/builds.toml
+
+hubsrht/services/git/__init__.py: $(GIT_GRAPHQL_QUERIES) ariadne/git.toml
+	$(ARIADNE_CODEGEN) --config ariadne/git.toml
+
+hubsrht/services/hg/__init__.py: $(HG_GRAPHQL_QUERIES) ariadne/hg.toml
+	$(ARIADNE_CODEGEN) --config ariadne/hg.toml
+
+hubsrht/services/lists/__init__.py: $(LISTS_GRAPHQL_QUERIES) ariadne/lists.toml
+	$(ARIADNE_CODEGEN) --config ariadne/lists.toml
+
+hubsrht/services/todo/__init__.py: $(TODO_GRAPHQL_QUERIES) ariadne/todo.toml
+	$(ARIADNE_CODEGEN) --config ariadne/todo.toml
+
+all-python: hubsrht/services/builds/__init__.py
+all-python: hubsrht/services/git/__init__.py
+all-python: hubsrht/services/hg/__init__.py
+all-python: hubsrht/services/lists/__init__.py
+all-python: hubsrht/services/todo/__init__.py
 
 install-bin: all-bin
 	mkdir -p $(BINDIR)
@@ -45,9 +77,24 @@ clean-bin:
 clean-share:
 	rm -f static/main.min.css static/main.css
 
-.PHONY: all all-bin all-share
+clean-python:
+	./scripts/clean-python builds
+	./scripts/clean-python git
+	./scripts/clean-python hg
+	./scripts/clean-python lists
+	./scripts/clean-python todo
+	# TODO: Replace the script with the following once legacy webhook
+	# support is removed:
+	# rm -rf hubsrht/services/builds/*.py hubsrht/services/builds/__pycache__
+	# rm -rf hubsrht/services/git/*.py hubsrht/services/git/__pycache__
+	# rm -rf hubsrht/services/hg/*.py hubsrht/services/hg/__pycache__
+	# rm -rf hubsrht/services/lists/*.py hubsrht/services/lists/__pycache__
+	# rm -rf hubsrht/services/todo/*.py hubsrht/services/todo/__pycache__
+	rm -f ariadne/*.toml
+
+.PHONY: all all-bin all-share all-python
 .PHONY: install install-bin install-share
-.PHONY: clean clean-bin clean-share
+.PHONY: clean clean-bin clean-share clean-python
 
 static/main.css: scss/main.scss
 	mkdir -p $(@D)

ariadne/builds.toml.in
@@ -1,0 +1,15 @@
+[tool.ariadne-codegen]
+async_client = false
+client_name = "BuildsClient"
+schema_path = "@ASSETS@/builds.sr.ht.graphqls"
+queries_path = "hubsrht/services/builds/"
+target_package_path = "hubsrht/services/"
+target_package_name = "builds"
+plugins = ["srht.ariadne.AriadnePlugin"]
+
+[tool.ariadne-codegen.sourcehut]
+service_name = "builds.sr.ht"
+
+[tool.ariadne-codegen.scalars.Time]
+type = "datetime.datetime"
+parse = "srht.graphql.gql_time"

ariadne/git.toml.in
@@ -1,0 +1,15 @@
+[tool.ariadne-codegen]
+async_client = false
+client_name = "GitClient"
+schema_path = "@ASSETS@/git.sr.ht.graphqls"
+queries_path = "hubsrht/services/git/"
+target_package_path = "hubsrht/services/"
+target_package_name = "git"
+plugins = ["srht.ariadne.AriadnePlugin"]
+
+[tool.ariadne-codegen.sourcehut]
+service_name = "git.sr.ht"
+
+[tool.ariadne-codegen.scalars.Time]
+type = "datetime.datetime"
+parse = "srht.graphql.gql_time"

commit 3be87aceff9ce5c71e734e1bb52a509dad347b1a

webhooks: properly set auth towards lists
Build status are not reflected in lists.sr.ht anymore after f15a6a03 due
to an oversight in that commit: we need to setup internal auth with the
project owner, not nothing. If we don't, then lists.sr.ht's UpdateTool
finds no rows to update in the database.

Fixes: f15a6a03 ("Use ariadne-codegen for GraphQL clients")
Signed-off-by: Simon Martin <simon@nasilyan.com>

hubsrht/blueprints/webhooks.py
@@ -523,7 +523,7 @@ def build_complete(details):
     builds_origin = get_origin("builds.sr.ht", external=True)
     build_url = f"{builds_origin}/{project.owner.canonical_name}/job/{payload['id']}"
 
-    lists_client = ListsClient()
+    lists_client = ListsClient(InternalAuth(project.owner))
     # TODO: Update me once builds.sr.ht adds a native enum for this
     match payload["status"]:
         case 'pending':