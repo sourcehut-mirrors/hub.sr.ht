sourcehut-mirrors/hub.sr.ht

commit 1678dec5c0f040f6891a439aba244fa13282a092

Replace remaining legacy webhooks with GQL webhooks

hubsrht/blueprints/projects.py
@@ -1,6 +1,3 @@
-import hubsrht.services.git.webhooks as git_webhooks # XXX Legacy webhooks
-import hubsrht.services.hg.webhooks as hg_webhooks # XXX Legacy webhooks
-import hubsrht.services.todo.webhooks as todo_webhooks # XXX Legacy webhooks
 import re
 import string
 from flask import Blueprint, Response, render_template, request, redirect, url_for
@@ -10,6 +7,7 @@ from hubsrht.projects import ProjectAccess, get_project, get_project_or_redir
 from hubsrht.services.git import GitClient
 from hubsrht.services.hg import HgClient
 from hubsrht.services.lists import ListsClient
+from hubsrht.services.todo import TodoClient
 from hubsrht.types import Feature, Event, EventType
 from hubsrht.types import Project, RepoType, Visibility
 from hubsrht.types import Redirect
@@ -365,7 +363,10 @@ def delete_POST(owner, project_name):
         abort(404)
     session["notice"] = f"{project.name} has been deleted."
 
+    git_client = GitClient()
+    hg_client = HgClient()
     lists_client = ListsClient()
+    todo_client = TodoClient()
 
     # Any mailing list, repository or tracker associated to the project will
     # be deleted via the foreign key it has on project.id; we need to clean-up
@@ -379,16 +380,14 @@ def delete_POST(owner, project_name):
         .filter(SourceRepo.project_id == project.id))
     for r in associated_repos:
         if r.repo_type == RepoType.git:
-            git_webhooks.unensure_user_webhooks(owner)
-            git_webhooks.unensure_repo_webhooks(r)
+            git_client.delete_repo_webhook(r.webhook_id)
         else:
-            hg_webhooks.unensure_user_webhooks(owner)
+            hg_client.delete_repo_webhook(r.webhook_id)
 
     associated_trackers = (Tracker.query
         .filter(Tracker.project_id == project.id))
     for t in associated_trackers:
-        todo_webhooks.unensure_user_webhooks(owner)
-        todo_webhooks.unensure_tracker_webhooks(t)
+        todo_client.delete_tracker_webhook(r.webhook_id)
 
     with db.engine.connect() as conn:
         conn.execute(text(f"DELETE FROM project WHERE id = {project.id}"))

.gitignore
@@ -19,9 +19,6 @@ api/graph/api/generated.go
 ariadne/*.toml
 hubsrht/services/builds/*.py
 hubsrht/services/git/*.py
-!hubsrht/services/git/webhooks.py
 hubsrht/services/hg/*.py
-!hubsrht/services/hg/webhooks.py
 hubsrht/services/lists/*.py
 hubsrht/services/todo/*.py
-!hubsrht/services/todo/webhooks.py

contrib/ensure-webhooks
@@ -5,8 +5,14 @@
 
 from flask import url_for
 from hubsrht.app import app
+from hubsrht.services.git import GitClient
+from hubsrht.services.hg import HgClient
 from hubsrht.services.lists import ListsClient
+from hubsrht.services.todo import TodoClient
 from hubsrht.blueprints.mailing_lists import LIST_WEBHOOK_VERSION
+from hubsrht.blueprints.sources import GIT_WEBHOOK_VERSION, HG_WEBHOOK_VERSION
+from hubsrht.blueprints.trackers import TODO_WEBHOOK_VERSION
+from hubsrht.webhooks import get_user_webhooks
 from srht.config import cfg, get_origin
 from srht.database import DbSession
 from srht.graphql import InternalAuth
@@ -15,24 +21,13 @@ connection_string = cfg("hub.sr.ht", "connection-string")
 db = DbSession(connection_string)
 db.create()
 
-from hubsrht.types import MailingList
+from hubsrht.types import User, MailingList, Tracker, UserWebhooks
+from hubsrht.types import SourceRepo, RepoType
 
 _listsrht = get_origin("lists.sr.ht", default=None)
-_hubsrht = get_origin("hub.sr.ht", default=None)
-
-def remove_previous_gql_webhook(client, mailing_list):
-    if (mailing_list.webhook_id == -1
-        and mailing_list.webhook_version == LIST_WEBHOOK_VERSION):
-        return
-
-    try:
-        client = ListsClient(auth=InternalAuth(mailing_list.owner))
-        client.delete_list_webhook(mailing_list.webhook_id)
-        mailing_list.webhook_id = -1
-        mailing_list.webhook_version = 0
-    except Exception as ex:
-        print(f"Warning: failed to remove previous webhook ID {mailing_list.webhook_id} for mailing list ID {mailing_list.id}")
-        print(ex)
+_todosrht = get_origin("todo.sr.ht", default=None)
+_gitsrht = get_origin("git.sr.ht", default=None)
+_hgsrht = get_origin("hg.sr.ht", default=None)
 
 print("Updating webhooks...")
 
@@ -43,28 +38,124 @@ if external_origin.startswith("https"):
 external_origin = external_origin.removeprefix("http://")
 external_origin = external_origin.removeprefix("https://")
 app.config['SERVER_NAME'] = external_origin
-with app.app_context():
-    for mailing_list in MailingList.query.all():
-        client = ListsClient(auth=InternalAuth(mailing_list.owner))
 
-        if (mailing_list.webhook_id != -1
-            and mailing_list.webhook_version == LIST_WEBHOOK_VERSION):
-            continue
+def up_to_date(have, want):
+    if have is None:
+        return False
+    return have >= want
 
-        if mailing_list.webhook_id != -1:
-            remove_previous_gql_webhook(client, mailing_list)
+def update_resource_hook(res, url, target_version, delete, create):
+    if up_to_date(res.webhook_version, target_version):
+        return
 
-        print(f"Update {mailing_list.owner.canonical_name}/{mailing_list.name} (ID: {mailing_list.remote_id})")
+    print(f"Update {res} to version {target_version}")
+    if res.webhook_id is not None:
         try:
-            webhook_url = url_for("webhooks.project_mailing_list", list_id=mailing_list.id)
-            mailing_list.webhook_id = client.create_list_webhook(
-                    list_id=mailing_list.remote_id,
-                    payload=ListsClient.event_webhook_query,
-                    url=webhook_url).webhook.id
-            mailing_list.webhook_version = LIST_WEBHOOK_VERSION
+            delete()
         except Exception as ex:
-            print(f"Warning: failed to update mailing list {mailing_list.remote_id}: {ex}")
+            print(f"Error deleting obsolete webhook for {res}: {ex}")
+
+    try:
+        res.webhook_id = create()
+        res.webhook_version = target_version
+    except Exception as ex:
+        print(f"Error creating webhook for {res}: {ex}")
 
     db.session.commit()
+
+with app.app_context():
+    for res in MailingList.query.all():
+        client = ListsClient(auth=InternalAuth(res.owner))
+        webhook_url = url_for("webhooks.project_mailing_list", list_id=res.id)
+        update_resource_hook(res, webhook_url, LIST_WEBHOOK_VERSION,
+            delete=lambda: client.delete_list_webhook(res.remote_id),
+            create=lambda: client.create_list_webhook(
+                list_id=res.remote_id,
+                payload=ListsClient.event_webhook_query,
+                url=webhook_url).webhook.id)
+
+    for user in User.query.join(MailingList).distinct():
+        uwh = get_user_webhooks(user)
+        if up_to_date(uwh.lists_webhook_version, LIST_WEBHOOK_VERSION):
+            continue
+        print(f"Configuring lists user webhook for {user}")
+        client = ListsClient(auth=InternalAuth(res.owner))
+        webhook_url = url_for("webhooks.mailing_list_user", user_id=user.id)
+        if uwh.lists_webhook_id is not None:
+            client.delete_user_webhook(uwh.lists_webhook_id)
+        uwh.lists_webhook_id = client.create_user_webhook(
+                payload=ListsClient.event_webhook_query,
+                url=webhook_url).webhook.id
+        uwh.lists_webhook_version = LIST_WEBHOOK_VERSION
+        db.session.commit()
+
+    for res in SourceRepo.query.filter(SourceRepo.repo_type == RepoType.git):
+        client = GitClient(auth=InternalAuth(res.owner))
+        webhook_url = url_for("webhooks.git_repo", repo_id=res.id)
+        update_resource_hook(res, webhook_url, GIT_WEBHOOK_VERSION,
+            delete=lambda: client.delete_repo_webhook(res.remote_id),
+            create=lambda: client.create_repo_webhook(
+                repo_id=res.remote_id,
+                payload=GitClient.event_webhook_query,
+                url=webhook_url).webhook.id)
+
+    for user in (User.query.join(SourceRepo)
+                 .filter(SourceRepo.repo_type == RepoType.git)
+                 .distinct()):
+        uwh = get_user_webhooks(user)
+        if up_to_date(uwh.git_webhook_version, GIT_WEBHOOK_VERSION):
+            continue
+        print(f"Configuring git user webhook for {user}")
+        client = GitClient(auth=InternalAuth(res.owner))
+        webhook_url = url_for("webhooks.git_user", user_id=user.id)
+        if uwh.git_webhook_id is not None:
+            client.delete_user_webhook(uwh.git_webhook_id)
+        uwh.git_webhook_id = client.create_user_webhook(
+                payload=GitClient.event_webhook_query,
+                url=webhook_url).webhook.id
+        uwh.git_webhook_version = GIT_WEBHOOK_VERSION
+        db.session.commit()
+
+    for user in (User.query.join(SourceRepo)
+                 .filter(SourceRepo.repo_type == RepoType.hg)
+                 .distinct()):
+        uwh = get_user_webhooks(user)
+        if up_to_date(uwh.hg_webhook_version, HG_WEBHOOK_VERSION):
+            continue
+        print(f"Configuring hg user webhook for {user}")
+        client = HgClient(auth=InternalAuth(res.owner))
+        webhook_url = url_for("webhooks.hg_user", user_id=user.id)
+        if uwh.hg_webhook_id is not None:
+            client.delete_user_webhook(uwh.hg_webhook_id)
+        uwh.hg_webhook_id = client.create_user_webhook(
+                payload=HgClient.event_webhook_query,
+                url=webhook_url).webhook.id
+        uwh.hg_webhook_version = HG_WEBHOOK_VERSION
+        db.session.commit()
+
+    for res in Tracker.query.all():
+        client = TodoClient(auth=InternalAuth(res.owner))
+        webhook_url = url_for("webhooks.todo_tracker", tracker_id=res.id)
+        update_resource_hook(res, webhook_url, TODO_WEBHOOK_VERSION,
+            delete=lambda: client.delete_tracker_webhook(res.remote_id),
+            create=lambda: client.create_tracker_webhook(
+                tracker_id=res.remote_id,
+                payload=TodoClient.event_webhook_query,
+                url=webhook_url).webhook.id)
+
+    for user in User.query.join(Tracker).distinct():
+        uwh = get_user_webhooks(user)
+        if up_to_date(uwh.todo_webhook_version, TODO_WEBHOOK_VERSION):
+            continue
+        print(f"Configuring todo user webhook for {user}")
+        client = TodoClient(auth=InternalAuth(res.owner))
+        webhook_url = url_for("webhooks.todo_user", user_id=user.id)
+        if uwh.todo_webhook_id is not None:
+            client.delete_user_webhook(uwh.todo_webhook_id)
+        uwh.todo_webhook_id = client.create_user_webhook(
+                payload=TodoClient.event_webhook_query,
+                url=webhook_url).webhook.id
+        uwh.todo_webhook_version = TODO_WEBHOOK_VERSION
+        db.session.commit()
 
 print("Done.")

hubsrht/blueprints/mailing_lists.py
@@ -5,6 +5,7 @@ from hubsrht.services.lists import ListsClient, Visibility as ListVisibility
 from hubsrht.types import Event, EventType
 from hubsrht.types import MailingList, Visibility
 from hubsrht.types.eventprojectassoc import EventProjectAssociation
+from hubsrht.webhooks import get_user_webhooks
 from srht.config import get_origin
 from srht.database import db
 from srht.flask import paginate_query
@@ -82,6 +83,15 @@ def finalize_add_list(client, owner, project, mailing_list):
             list_id=ml.remote_id,
             payload=ListsClient.event_webhook_query,
             url=webhook_url).webhook.id
+
+    uwh = get_user_webhooks(owner)
+    if uwh.lists_webhook_id is None:
+        user_webhook_url = (get_origin("hub.sr.ht", external=False) +
+               url_for("webhooks.mailing_list_user", user_id=owner.id))
+        uwh.lists_webhook_id = client.create_user_webhook(
+                payload=ListsClient.event_webhook_query,
+                url=user_webhook_url).webhook.id
+        uwh.lists_webhook_version = LIST_WEBHOOK_VERSION
 
     event = Event()
     event.event_type = EventType.mailing_list_added 

hubsrht/blueprints/sources.py
@@ -1,5 +1,3 @@
-import hubsrht.services.git.webhooks as git_webhooks # XXX Legacy webhooks
-import hubsrht.services.hg.webhooks as hg_webhooks # XXX Legacy webhooks
 from flask import Blueprint, render_template, request, redirect, url_for, abort
 from hubsrht.projects import ProjectAccess, get_project, get_project_or_redir
 from hubsrht.services.hg import HgClient, Visibility as HgVisibility
@@ -7,6 +5,7 @@ from hubsrht.services.git import GitClient, Visibility as GitVisibility
 from hubsrht.types import Event, EventType
 from hubsrht.types import RepoType, SourceRepo, Visibility
 from hubsrht.types.eventprojectassoc import EventProjectAssociation
+from hubsrht.webhooks import get_user_webhooks
 from srht.config import get_origin
 from srht.database import db
 from srht.flask import paginate_query
@@ -15,6 +14,9 @@ from srht.search import search_by
 from srht.validation import Validation
 
 sources = Blueprint("sources", __name__)
+
+GIT_WEBHOOK_VERSION = 1
+HG_WEBHOOK_VERSION = 1
 
 def get_repos(owner, project, repo_type):
     match repo_type:
@@ -139,8 +141,26 @@ def git_new_POST(owner, project_name):
     repo.description = git_repo.description
     repo.repo_type = RepoType.git
     repo.visibility = Visibility(git_repo.visibility.value)
+    repo.webhook_id = -1
+    repo.webhook_version = GIT_WEBHOOK_VERSION
     db.session.add(repo)
     db.session.flush()
+
+    webhook_url = (get_origin("hub.sr.ht", external=False) +
+           url_for("webhooks.git_repo", repo_id=repo.id))
+    repo.webhook_id = git_client.create_repo_webhook(
+            repo_id=repo.remote_id,
+            payload=GitClient.event_webhook_query,
+            url=webhook_url).webhook.id
+
+    uwh = get_user_webhooks(owner)
+    if uwh.git_webhook_id is None:
+        user_webhook_url = (get_origin("hub.sr.ht", external=False) +
+               url_for("webhooks.git_user", user_id=owner.id))
+        uwh.git_webhook_id = git_client.create_user_webhook(
+                payload=GitClient.event_webhook_query,
+                url=user_webhook_url).webhook.id
+        uwh.git_webhook_version = GIT_WEBHOOK_VERSION
 
     event = Event()
     event.event_type = EventType.source_repo_added
@@ -153,9 +173,6 @@ def git_new_POST(owner, project_name):
     assoc.event_id = event.id
     assoc.project_id = project.id
     db.session.add(assoc)
-
-    git_webhooks.ensure_user_webhooks(owner)
-    git_webhooks.ensure_repo_webhooks(repo)
 
     db.session.commit()
 
@@ -211,8 +228,19 @@ def hg_new_POST(owner, project_name):
     repo.description = hg_repo.description
     repo.repo_type = RepoType.hg
     repo.visibility = Visibility(hg_repo.visibility.value)
+    repo.webhook_id = -1
+    repo.webhook_version = HG_WEBHOOK_VERSION
     db.session.add(repo)
     db.session.flush()
+
+    uwh = get_user_webhooks(owner)
+    if uwh.hg_webhook_id is None:
+        user_webhook_url = (get_origin("hub.sr.ht", external=False) +
+               url_for("webhooks.hg_user", user_id=owner.id))
+        uwh.hg_webhook_id = hg_client.create_user_webhook(
+                payload=HgClient.event_webhook_query,
+                url=user_webhook_url).webhook.id
+        uwh.hg_webhook_version = HG_WEBHOOK_VERSION
 
     event = Event()
     event.event_type = EventType.source_repo_added
@@ -225,9 +253,6 @@ def hg_new_POST(owner, project_name):
     assoc.event_id = event.id
     assoc.project_id = project.id
     db.session.add(assoc)
-
-    hg_webhooks.ensure_user_webhooks(owner)
-    #hg_webhooks.ensure_repo_webhooks(repo) # TODO
 
     db.session.commit()
 
@@ -303,8 +328,14 @@ def delete_POST(owner, project_name, repo_id):
         project.summary_repo_id = None
         db.session.commit()
 
+    match repo.repo_type:
+        case RepoType.git:
+            client = GitClient()
+        case RepoType.hg:
+            client = HgClient()
+
     if repo.repo_type == RepoType.git:
-        git_webhooks.unensure_repo_webhooks(repo)
+        client.delete_repo_webhook(repo.webhook_id)
 
     repo_name = repo.name
     repo_id = repo.remote_id
@@ -314,11 +345,6 @@ def delete_POST(owner, project_name, repo_id):
     valid = Validation(request)
     delete_remote = valid.optional("delete-remote") == "on"
     if delete_remote:
-        match repo.repo_type:
-            case RepoType.git:
-                client = GitClient()
-            case RepoType.hg:
-                client = HgClient()
         client.delete_repo(repo_id)
 
     return redirect(url_for("projects.summary_GET",

hubsrht/blueprints/trackers.py
@@ -1,10 +1,11 @@
-import hubsrht.services.todo.webhooks as todo_webhooks # XXX Legacy webhooks
 from flask import Blueprint, render_template, request, redirect, url_for
 from flask import abort
 from hubsrht.projects import ProjectAccess, get_project, get_project_or_redir
 from hubsrht.services.todo import TodoClient, Visibility as TrackerVisibility
 from hubsrht.types import Event, EventType, Tracker, Visibility
 from hubsrht.types.eventprojectassoc import EventProjectAssociation
+from hubsrht.webhooks import get_user_webhooks
+from srht.config import get_origin
 from srht.database import db
 from srht.flask import paginate_query
 from srht.oauth import current_user, loginrequired
@@ -12,6 +13,8 @@ from srht.search import search_by
 from srht.validation import Validation
 
 trackers = Blueprint("trackers", __name__)
+
+TODO_WEBHOOK_VERSION = 1
 
 @trackers.route("/<owner>/<project_name>/trackers")
 def trackers_GET(owner, project_name):
@@ -108,8 +111,27 @@ def new_POST(owner, project_name):
     tracker.name = remote_tracker.name
     tracker.description = remote_tracker.description
     tracker.visibility = Visibility(remote_tracker.visibility.value)
+    tracker.webhook_id = -1
+    tracker.webhook_version = TODO_WEBHOOK_VERSION
     db.session.add(tracker)
     db.session.flush()
+
+    webhook_url = (get_origin("hub.sr.ht", external=False) +
+           url_for("webhooks.todo_tracker", tracker_id=tracker.id))
+    tracker.webhook_id = todo_client.create_tracker_webhook(
+            tracker_id=tracker.remote_id,
+            payload=TodoClient.event_webhook_query,
+            url=webhook_url).webhook.id
+    tracker.webhook_version = TODO_WEBHOOK_VERSION
+
+    uwh = get_user_webhooks(owner)
+    if uwh.todo_webhook_id is None:
+        user_webhook_url = (get_origin("hub.sr.ht", external=False) +
+               url_for("webhooks.todo_user", user_id=owner.id))
+        uwh.todo_webhook_id = todo_client.create_user_webhook(
+                payload=TodoClient.event_webhook_query,
+                url=user_webhook_url).webhook.id
+        uwh.todo_webhook_version = TODO_WEBHOOK_VERSION
 
     event = Event()
     event.event_type = EventType.tracker_added
@@ -122,9 +144,6 @@ def new_POST(owner, project_name):
     assoc.event_id = event.id
     assoc.project_id = project.id
     db.session.add(assoc)
-
-    todo_webhooks.ensure_user_webhooks(owner)
-    todo_webhooks.ensure_tracker_webhooks(tracker)
 
     db.session.commit()
 
@@ -182,12 +201,13 @@ def delete_POST(owner, project_name, tracker_id):
     db.session.delete(tracker)
     db.session.commit()
 
-    todo_webhooks.unensure_tracker_webhooks(tracker)
+    client = TodoClient()
+    client.delete_tracker_webhook(tracker.webhook_id)
 
     valid = Validation(request)
     delete_remote = valid.optional("delete-remote") == "on"
     if delete_remote:
-        TodoClient().delete_tracker(remote_id)
+        client.delete_tracker(remote_id)
 
     return redirect(url_for("projects.summary_GET",
         owner=owner.canonical_name, project_name=project.name))

hubsrht/services/git/queries.graphql
@@ -77,22 +77,6 @@ query GetManifests($username: String!, $repo_name: String!) {
   }
 }
 
-query GetLog($username: String!, $repo: String!, $from: String!) {
-  user(username: $username) {
-    repository(name: $repo) {
-      log(from: $from) {
-        results {
-          id
-          message
-          author {
-            name
-          }
-        }
-      }
-    }
-  }
-}
-
 mutation CreateRepo(
   $name: String!,
   $visibility: Visibility!,
@@ -112,4 +96,91 @@ mutation CreateRepo(
 
 mutation DeleteRepo($repo_id: Int!) {
   deleteRepository(id: $repo_id) { id }
+}
+
+mutation CreateUserWebhook(
+  $payload: String!
+  $url: String!,
+) {
+  webhook: createUserWebhook(config: {
+    url: $url,
+    events: [
+      REPO_UPDATE,
+      REPO_DELETED,
+    ],
+    query: $payload
+  }) { id }
+}
+
+mutation DeleteUserWebhook($id: Int!) {
+  deleteUserWebhook(id: $id) { id }
+}
+
+mutation CreateRepoWebhook(
+  $repoID: Int!,
+  $payload: String!,
+  $url: String!,
+) {
+  webhook: createGitWebhook(config: {
+    repositoryID: $repoID,
+    url: $url,
+    query: $payload,
+    events: [GIT_POST_RECEIVE],
+  }) { id }
+}
+
+mutation DeleteRepoWebhook($id: Int!) {
+  deleteGitWebhook(id: $id) { id }
+}
+
+query EventWebhook {
+  webhook {
+    uuid
+    event
+    date
+
+    ... on RepositoryEvent {
+      repository {
+        id
+        name
+        description
+        visibility
+      }
+    }
+
+    ... on GitEvent {
+      pusher {
+        canonicalName
+
+        ... on User {
+          username
+        }
+      }
+
+      updates {
+        old {
+          id
+        }
+
+        new {
+          id
+          shortId
+
+          ... on Commit {
+            message
+          }
+        }
+
+        log {
+          results {
+            id
+            message
+            author {
+              name
+            }
+          }
+        }
+      }
+    }
+  }
 }

commit 7f803988740e8e58cb29372f100fd1093db7981f

projects: fix regressions from 1678dec5
I noticed two issues by running sr.ht-integration-testing on master,
that this patch fixes:
 - We unconditionally instantiate a HgClient, which will fail on
   installations without Mercurial, and is actually not necessary after
   a7d268db.
 - We try to delete tracker webhooks from **repository** webhook IDs...

Fixes: 1678dec5 ("Replace remaining legacy webhooks with GQL webhooks")
Fixes: a7d268db ("Allow for nullable webhook IDs for hg repos")
Signed-off-by: Simon Martin <simon@nasilyan.com>

hubsrht/blueprints/projects.py
@@ -364,7 +364,6 @@ def delete_POST(owner, project_name):
     session["notice"] = f"{project.name} has been deleted."
 
     git_client = GitClient()
-    hg_client = HgClient()
     lists_client = ListsClient()
     todo_client = TodoClient()
 
@@ -385,7 +384,7 @@ def delete_POST(owner, project_name):
     associated_trackers = (Tracker.query
         .filter(Tracker.project_id == project.id))
     for t in associated_trackers:
-        todo_client.delete_tracker_webhook(r.webhook_id)
+        todo_client.delete_tracker_webhook(t.webhook_id)
 
     with db.engine.connect() as conn:
         conn.execute(text(f"DELETE FROM project WHERE id = {project.id}"))